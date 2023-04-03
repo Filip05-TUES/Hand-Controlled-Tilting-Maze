f3a40263909c31e91022668a771a850cd82fc5cc
Filip05-TUES <72618444+Filip05-TUES@users.noreply.github.com>
2023-04-03 22:52:05 +0300

    Add files via upload

---
+++ sketch_feb27a/sketch_feb27a.ino
@@ -0,0 +1,53 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+Adafruit_MPU6050 srituhobby;
+RF24 radio(7, 8);
+const byte address[6] = "00001";
+
+float prevX;
+float smoothX;
+float prevY;
+float smoothY;
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+  Wire.begin();
+  srituhobby.begin();
+
+  srituhobby.setAccelerometerRange(MPU6050_RANGE_8_G);//2_G,4_G,8_G,16_G
+  srituhobby.setGyroRange(MPU6050_RANGE_500_DEG);//250,500,1000,2000
+  srituhobby.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  delay(100);
+}
+
+void loop() {
+  sensors_event_t a, g, temp;
+  srituhobby.getEvent(&a, &g, &temp);
+
+  //double variableX = (a.acceleration.x + 10) / 20;
+  //double variableY = (a.acceleration.y + 10) / 20;
+
+  int valueX = map(a.acceleration.x,  -10, 10, 180, 0);
+  int valueY = map(a.acceleration.y,  -10, 10, 180, 0);
+
+  smoothX = valueX * 0.05 + prevX * 0.95;
+  smoothY = valueY * 0.05 + prevY * 0.95;
+  prevX = smoothX;
+  prevY = smoothY;
+  
+  int angles[] = {smoothX, smoothY};
+  Serial.println(smoothX);
+  radio.write(&angles, sizeof(angles));
+  //delay(1000);
+}
---
+++ sketch_oct14a/sketch_oct14a.ino
@@ -0,0 +1,33 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(9, 10);
+const byte address[6] = "00001";
+Servo servo;
+Servo servo2;
+
+void setup(void) {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+  servo.attach(6);
+  servo2.attach(8);
+  servo.write(90);
+  servo2.write(90);
+}
+
+void loop() {
+  if (radio.available()) {
+    int angles[2];
+    radio.read(&angles, sizeof(angles));
+    //servo.write(angles[0]);
+    servo2.write(angles[1]);
+  }
+}
